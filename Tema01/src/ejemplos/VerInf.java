27643b38dbc2c5864dedf45b30afb266aaaeae18
JesusRuiver <jesusruiver@jesusruiver.com>
2017-10-02 18:48:47 +0200

    Path VerInf

--- Tema01/src/ejemplos/VerInf.java
+++ Tema01/src/ejemplos/VerInf.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class VerInf {
 public static void main(String[] args) {
   System.out.println("INFORMACIÓN SOBRE EL FICHERO:");
-  File f = new File("VerInf.java");
+  File f = new File("C:/Eclipse_Neon/Eclipse Jesús DAM 2/ReporsitorioLocal/Tema01/Tema01/src/ejemplos/VerInf.java");
   if(f.exists()){
         System.out.println("Nombre del fichero  : "+f.getName());
         System.out.println("Ruta                : "+f.getPath());
