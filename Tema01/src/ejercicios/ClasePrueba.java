157a3a5aae2d10bf41330192a956bd7811584d71
JesusRuiver <jesusruiver@jesusruiver.com>
2017-10-02 18:03:27 +0200

    Commit clase prueba

---
+++ Tema01/src/ejercicios/ClasePrueba.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicios;
+
+/**
+ * @author Vespertino
+ *
+ */
+public class ClasePrueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
