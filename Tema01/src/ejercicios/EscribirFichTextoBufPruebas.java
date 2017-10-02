0570aa0ae822d03a273886d793909c063c2fe7b7
JesusRuiver <jesusruiver@jesusruiver.com>
2017-10-02 18:17:39 +0200

    Ejercicios de pruebas

--- Tema01/src/ejercicios/EscribirFichTextoBufPruebas.java
+++ Tema01/src/ejercicios/EscribirFichTextoBufPruebas.java
@@ -1,13 +1,14 @@
-package ejemplos;
+package ejercicios;
 
 import java.io.*;
-public class EscribirFichTextoBuf {
+public class EscribirFichTextoBufPruebas {
   public static void main(String[] args) {
   try{      
       BufferedWriter fichero = new BufferedWriter(new FileWriter("FichTexto.txt"));
       for (int i=1; i<11; i++){
 	    fichero.write("Fila numero: "+i); //escribe una línea
 	    fichero.newLine(); //escribe un salto de línea
+	    fichero.flush();
       }
       fichero.close(); 
       System.out.println("Fichero creado");
