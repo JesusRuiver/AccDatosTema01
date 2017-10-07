0b59d3ac80d57e87df8e4e171ed317d4a177dc58
JesusRuiver <jesusruiver@jesusruiver.com>
2017-10-07 20:55:15 +0200

    ejercicio departamentos

---
+++ Tema01/src/po0102/Departamento.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package po0102;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * @author Jesús Manuel Ruiz Verdejo
+ *
+ */
+public class Departamento {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		/*
+		 * Realiza un programa en Java que cree un fichero binario para guardar
+		 * datos de departamentos, dale el nombre de Departamentos.dat
+		 * 
+		 * Introduce al menos 10 departamentos. Los Datos por cada departamento
+		 * son: Número Departamento: int, Nombre: String y Localidad: String.
+		 */
+
+		int numDep;
+		String nombre;
+		String localidad;
+
+		String[] nombres = { "Contabilidad", "Ventas", "Logística", "RRHH", "Informática", "Exportación", "Calidad",
+				"Lega", "Comunicación", "Diseño" };
+		
+		String[] localidades = { "Zaragoza", "Bilbao", "Valencia", "Barcelona", "La Coruña", "Sevilla", "Cordoba",
+				"Madrid", "Murcia", "Castellón" };
+		
+		int[] numsDeps = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		File ficheroDep = new File("Departamentos.dat");
+		FileOutputStream flujoDatos = new FileOutputStream(ficheroDep);
+		DataOutputStream escribeDatos = new DataOutputStream(flujoDatos);
+
+		for (int i = 0; i < numsDeps.length; i++) {
+				
+			escribeDatos.writeInt(numsDeps[i]);
+			escribeDatos.writeUTF(nombres[i]);
+			escribeDatos.writeUTF(localidades[i]);
+		}
+		
+		escribeDatos.close();
+	}
+
+}
