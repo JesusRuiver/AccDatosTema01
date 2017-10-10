ba526d072d9e7b5f4708529a1fd4de7c10758e1b
Jesús <jesusruiver@jesusruiver.com>
2017-10-10 12:58:49 +0200

    Reazlizando ejercicicios

--- Tema01/src/ejercicio1punto3/ConsultaDatos.java
+++ Tema01/src/ejercicio1punto3/ConsultaDatos.java
@@ -3,18 +3,51 @@
  */
 package ejercicio1punto3;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
- * @author Vespertino
+ * @author Jesús Manuel Ruiz Verdejo
  *
  */
 public class ConsultaDatos {
 
 	/**
 	 * @param args
+	 * @throws Exception
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
+		String idEmple = args[0];
+
+		File fichero = new File("AleatorioEmpleEjercicio.dat");
+
+		// declara el fichero de acceso aleatorio a leer
+
+		RandomAccessFile file = new RandomAccessFile(fichero, "r");
+
+		int id, dep, posicion;
+		Double salario;
+		char apellido[] = new char[10], aux;
+
+		posicion = (Integer.parseInt(idEmple) - 1) * 36; // para situarnos al principio
+
+		if (posicion >= file.length()) { 
+			
+			System.out.printf("ID: %d, NO EXISTE EMPLEADO...", Integer.parseInt(idEmple));
+			
+		}else {// recorro el fichero
+			file.seek(posicion); // nos posicionamos en posicion
+			id = file.readInt(); // obtengo id de empleado
+				
+			System.out.println(
+					"ID: " + id);
+				
+		
+		} // fin bucle for
+		file.close(); // cerrar fichero
 	}
 
 }
+
--- Tema01/src/ejercicio1punto3/InsertaDatos.java
+++ Tema01/src/ejercicio1punto3/InsertaDatos.java
@@ -3,18 +3,31 @@
  */
 package ejercicio1punto3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 /**
- * @author Vespertino
+ * @author Jesús Manuel Ruiz Verdejo
  *
  */
 public class InsertaDatos {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		String idEmple = args [0];
+		String apellido = args [1];
+		String departamento = args [2];
+		String salario = args[3];
+		
+		File fichero = new File("AleatorioEmpleEjercicio.dat");
+		
+		RandomAccessFile file = new RandomAccessFile(fichero, "rw");
+		
+		
 	}
 
 }
